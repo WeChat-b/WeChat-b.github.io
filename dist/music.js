f1b1ec3b73dd1a1d59df3dfa507cc1994c631143
WeChat-b <201423752@qq.com>
2019-12-05 20:05:28 +0800

    Site updated: 2019-12-05 20:05:28

---
+++ dist/music.js
@@ -0,0 +1,9 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    audio: [{
+        name: 'name',
+        artist: 'artist',
+        url: '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=1360028990&auto=1&height=66"></iframe>',
+        cover: 'cover.jpg'
+    }]
+});
