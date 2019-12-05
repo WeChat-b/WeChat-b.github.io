bc5340103f29f547e3efdcc51d6654c31a0da5ef
WeChat-b <201423752@qq.com>
2019-12-05 20:10:25 +0800

    Site updated: 2019-12-05 20:10:24

--- dist/music.js
+++ dist/music.js
@@ -1,9 +1,10 @@
 const ap = new APlayer({
-    container: document.getElementById('aplayer'),
+    container: document.getElementById('player'),
+    fixed: true,
     audio: [{
-        name: 'name',
-        artist: 'artist',
+        name: 'V6-迪迦Take Me Higher',
+        artist: 'Bonca金柱',
         url: '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=1360028990&auto=1&height=66"></iframe>',
-        cover: 'cover.jpg'
+        cover: 'cover.jpg',
     }]
 });
